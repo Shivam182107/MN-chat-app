fbc56f088130a8de2e532f927f24f02399b7167d
Shivam Ranjan Moharana <shivamranjanmoharana@gmail.com>
2026-05-20 18:16:31 +0530

    Adding new database cluster

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose")
 
 async function connectDb(){
     // await mongoose.connect(process.env.DB_URL);
-    await mongoose.connect("mongodb://127.0.0.1:27017/chatdb");
+    await mongoose.connect(process.env.DB_URL);
 
 }
 module.exports=connectDb
